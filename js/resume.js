62382f83ff55978792e3b3801589e414bfb5f87a
Chris Bednar <bednar.christopher@gmail.com>
2018-08-25 01:07:48 -0400

    update FAQ by adding links

--- js/resume.js
+++ js/resume.js
@@ -37,11 +37,11 @@
     },
     { 
       "question": "What was your diet like during your transformation?", 
-      "answer": "I mostly counted calories (3 meals a day that totaled 1800-2000 calories). I don't reccomend that hardcore calorie counting approach anymore. The best approach is to slowly change your dietary habits by eating the right foods. (in a nutshell)"
+      "answer": "I mostly <a href='https://youtu.be/_ClkHfNrQ6o'>counted calories</a> (3 meals a day that totaled 1800-2000 calories). <a href='https://youtu.be/S1ufbGs6uuY'>I don't recommend that hardcore calorie counting approach anymore</a>. The best approach is to slowly change your dietary habits by eating the right foods. (in a nutshell)"
     },
         { 
       "question": "What was your workout routine like during your transformation?", 
-      "answer": "I would walk for at least 20 minutes everyday and I did a 1 hour gym workout 3 days a week. Back & biceps for one day. Chest & triceps for another day and legs &amp; shoulders &amp; abs for the last day."
+      "answer": "I would walk for at least 20 minutes everyday and I did a 1 hour gym workout 3 days a week. <a href='https://youtu.be/fdy67B77X6A'>Back & biceps</a> for one day. <a href='https://youtu.be/pq8gV9-rcgw'>Chest & triceps</a> for another day and legs &amp; shoulders &amp; abs for the last day."
     },
     { 
       "question": "How tall are you?", 
@@ -60,7 +60,7 @@
       "answer": "Just drink coffee. Pre-workouts should be used rarely, like in cases when you are really tired."
     },
     {
-      "question": "What do you think about protein powders?", 
+      "question": "What do you think about <a href='https://www.youtube.com/watch?v=DvD5Nofmw5c'>protein powders</a>?", 
       "answer": "You should get most of your protein from food, but they aren't the worst thing for you. Don't rely on them heavily; have one after a workout if you must. Make sure to get a plant-based protein powder though as they are better for your health and the animals. You also don't need as much protein as you think you need."
     },
     {
